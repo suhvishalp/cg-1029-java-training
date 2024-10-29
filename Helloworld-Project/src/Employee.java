6fd418b0271b2565dc594c3bbcbb129b121f5ec6
vishalshah <suhvishal@gmail.com>
2024-10-29 12:33:06 +0530

    first commit

---
+++ Helloworld-Project/src/Employee.java
@@ -0,0 +1,20 @@
+
+public class Employee {
+
+	public int empId;			//instance variables / fields / data members 
+	public String name;
+	public double salary;
+	
+	public static String companyName;
+	
+	
+	public void showEmployee() {
+				
+		 
+		
+		System.out.println("Emp ID: " + empId);
+		System.out.println("Employee Name : "+ name);
+		System.out.println("Salary : "+ salary);
+	}
+	
+}
---
+++ Helloworld-Project/src/Helloworld.java
@@ -0,0 +1,16 @@
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee emp1 = new Employee();
+		
+		emp1.showEmployee();
+	}
+
+	
+	public static void sayHello() {
+		int a = 20;
+	}
+}
